eea4b814b70764cadeba961cf59b4014a6b3318a
Brandon Gioggia <bgioggia@gmail.com>
2018-08-17 17:10:15 -0400

    Added Functionality to defined handlers
    Some handlers have functionality that is only temporary and will change drastically by the final version of the project. This is because this is my first time using AWS lambda and am using some iterations of handlers to test and learn as I go.
    
    *VARIABLE CHANGES*
    'row1', 'row2', and 'row3' start off as ["blank", "blank", "blank",]
    'requestedShape' and 'requestedPosition' are made to take user input and change the board.
    'boardState' is used to store a string that is read by Alexa to tell the user what the board looks like.
    
    *FUNCTION CHANGES*
    'boardToString' changes the value of boardState to reflect the values of the board
    'changeBoard' changes the values of the board based on the values of 'requestedShape' and 'requestedPosition'
    'changeBoardX' and 'changeBoardO' add their respective symbol to the given position.
    
    *HANDLER CHANGES*
    'LaunchRequest' Welcomes the user and asks what they would like to do.
    'MakeMove' assigns user input values to 'requestedShape' and 'requestedPosition' then calls 'changeBoard' based on these values. Alexa then reads out the change that has been made to the user.
    'ReadBoard' runs the 'boardToString' function then speaks 'boardState'

--- Alexa-Tic-Tac-Toe/Tic-Tac-Toe.js
+++ Alexa-Tic-Tac-Toe/Tic-Tac-Toe.js
@@ -9,11 +9,66 @@ var row1 = ["blank", "blank", "blank"];
 var row2 = row1;
 var row3 = row1;
 
-var handlers = {
+//
+var requestedShape = "";
+var requestedPosition = "";
+    
+//String that alexa uses to read the state of the board to the user. 
+var boardState= ""
 
-  //Handler for app startup
-  'LaunchRequest': function()
+//boardToString prints out the board in a sentence for Alexa to read.
+function boardToString()
+{
+  var x;
+  for (x in row1)
   {
+    boardState = boardState + row1[x] + " ";
+  }
+}
+
+//changeBoard calls a helper function depending on if an X or O is being added.
+function changeBoard(shape, position)
+{
+  if (shape == "o"){
+    changeBoardO(position);
+  }
+  else{
+    changeBoardX(position);
+  }
+}
+
+//adds O's to the position that the user requested
+function changeBoardO(position){
+  if(position == "top right"){  
+    row1[2] = "O";
+  }
+  else if(position == "top middle"){
+    row1[1] = "O";
+  }
+  else if(position == "top left"){
+    row1[0] = "O";
+  }
+}
+
+//adds X's to the position that the user requested
+function changeBoardX(position){
+  if(position == "top right"){  
+    row1[2] = "X";
+  }
+  else if(position == "top middle"){
+    row1[1] = "X";
+  }
+  else if(position == "top left"){
+    row1[0] = "X";
+  }
+  
+}
+
+var handlers = {
+  //Handler for app startup
+  'LaunchRequest': function() {
+    this.response.speak("Welcome to Tic Tac Toe! Would you like to start a new game?").listen("What would you like to do?");
+    this.emit(':responseReady');
   },
 
   //Handler for starting new game with blank board
@@ -26,9 +81,22 @@ var handlers = {
   {
   },
 
-  //Handler for adding X's and O's to the game board
+  //Handler for adding X's and O's to the board
   'MakeMove': function()
   {
+    requestedShape = this.event.request.intent.slots.shape.value;
+    requestedPosition = this.event.request.intent.slots.position.value;
+    changeBoard(requestedShape, requestedPosition);
+    this.response.speak("I have added an " + requestedShape + " to " + requestedPosition).listen("test");
+    this.emit(":responseReady");
+  },
+  
+  //Handler for reading out the current contents of the board
+  'ReadBoard': function()
+  {
+    boardToString();
+    this.response.speak(boardState);
+    this.emit(":responseReady");
   },
 
 }
